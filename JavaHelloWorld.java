5e0c22a90b455c171f308555ec9a7b99ed4e0fcd
pragna27 <peggi.pragna93@gmail.com>
2016-06-22 12:54:53 +0530

    Create JavaHelloWorld.java

---
+++ JavaHelloWorld.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+      public static void main (String [] args)
+      {
+           System.out.println("JavaHelloWorld");
+           System.out.println("Hello again");
+      }
+
+}
